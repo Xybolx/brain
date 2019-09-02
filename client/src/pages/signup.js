135f732de8898a7c0e51e62545a173b8b833cf59
Xybolx <xybolx@gmail.com>
2019-09-01 21:27:11 -0700

    pageLogo fixes

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -6,11 +6,9 @@ import PageLogo from '../components/pageLogo';
 const SignUp = () => {
 
     return (
-        <div style={{ marginTop: 30 }}>
+        <div className="page-container" style={{ marginTop: 30 }}>
+            <PageLogo stackSize="8" />
             <Col sm="12" md={{ size: 6, offset: 3 }}>
-                <PageLogo
-                    stackSize="8"
-                />
                 <SignUpForm />
             </Col>
         </div>
